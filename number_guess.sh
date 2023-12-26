6d32cc73e165979a80a9b8911b6525d10f6ce705
vzsoares <jogolots2@gmail.com>
2023-12-26 18:41:44 -0500

    feat: add output msg

--- number_guess.sh
+++ number_guess.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+
+OUTPUT_SUCCESS(){
+  # <number_of_guesses>
+  #<secret_number>
+  echo "You guessed it in $1 tries. The secret number was $2. Nice job!"
+}
