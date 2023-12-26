aa42c89473fe8ff15db9bdc35f1e46b9f41d0d5b
vzsoares <jogolots2@gmail.com>
2023-12-26 18:44:46 -0500

    feat: main func

--- number_guess.sh
+++ number_guess.sh
@@ -7,3 +7,12 @@ OUTPUT_SUCCESS(){
   #<secret_number>
   echo "You guessed it in $1 tries. The secret number was $2. Nice job!"
 }
+
+MAIN(){
+  echo "Enter your username:"
+  read NAME
+}
+
+echo -e "\n~~ Number guessing game ~~\n"
+
+MAIN
