a73826168e3315fa097817fb0901376ca4984a0a
vzsoares <jogolots2@gmail.com>
2023-12-26 19:14:28 -0500

    feat: insert game

--- number_guess.sh
+++ number_guess.sh
@@ -15,8 +15,21 @@ GEN_RANDOM(){
 MAIN(){
   echo "Enter your username:"
   read NAME
-  DB_USER=$($PSQL "SELECT user_id,name FROM users WHERE name = '$NAME'")
-  echo $DB_USER
+  USER_ID=$($PSQL "SELECT user_id FROM users WHERE name = '$NAME'")
+  if [[ -z $USER_ID ]]
+  then
+    INSERT_USER=$($PSQL "INSERT INTO users(name) VALUES('$NAME') RETURNING user_id")
+    USER_ID=$($INSERT_USER | cut -d' ' -f 1)
+    echo "Welcome, $NAME! It looks like this is your first time here."
+  else
+    GAMES_PLAYED=$($PSQL "SELECT COUNT(game_id) FROM games WHERE user_id = $USER_ID")
+    BEST_GAME=$($PSQL "SELECT attempts FROM games WHERE user_id = $USER_ID ORDER BY attempts DESC LIMIT 1")
+    echo "Welcome back, $NAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+  fi
+  SECRET_NUMBER=$(GEN_RANDOM)
+  INSERT_GAME=$($PSQL "INSERT INTO games(secret_number,user_id) VALUES($SECRET_NUMBER, $USER_ID)")
+
+  echo "Guess the secret number between 1 and 1000:"
 }
 
 echo -e "\n~~ Number guessing game ~~\n"
